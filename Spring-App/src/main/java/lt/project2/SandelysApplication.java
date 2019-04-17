07df0ee73ffd49da7dd700630477194ae074621c
AndrejusN <andrejus.n@gmail.com>
2019-04-17 14:16:47 +0300

    Added sample products to the database via @Bean in the SandelysApplication. Now they are created everytime Spring loads up. Maybe useful to edit them to match real life products.

--- Spring-App/src/main/java/lt/project2/SandelysApplication.java
+++ Spring-App/src/main/java/lt/project2/SandelysApplication.java
@@ -1,5 +1,8 @@
 package lt.project2;
 
+import lt.project2.model.ProductModel;
+import lt.project2.productRepository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +18,37 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class SandelysApplication {
 
+
+	/*	public ProductModel(String sku, int qty, double height, double weight, double length, double width) {
+		super();
+		this.sku = sku;
+		this.qty = qty;
+		this.height = height;
+		this.weight = weight;
+		this.length = length;
+		this.width = width;
+	}*/
+
+
+	@Bean
+	public CommandLineRunner demo(ProductRepository productRepository) {
+		return (args) -> {
+			// make entries to database
+					// edit with real-life products?
+			productRepository.save(new ProductModel("AP3358", 25, 13.0, 0.5, 0.7, 16));
+			productRepository.save(new ProductModel("ZT-9944", 1, 1.00, 3.00, 60.0, 5460.0));
+			productRepository.save(new ProductModel("00_a", 122, 13.00, 65.00, 111.0, 50.0));
+			productRepository.save(new ProductModel("35-aW!",9, 10.03, 30.00, 660.0, 350.0));
+			productRepository.save(new ProductModel("??????", 71, 08.00, 33., 67.0, 99.0));
+			productRepository.save(new ProductModel("PO233", 206, 54.00, 34.00, 787.0, 90.0));
+			productRepository.save(new ProductModel("NULL", 0, 44.00, 457.00, 60.0, 17.0));
+
+		};
+	}
+
 	@Bean
 	public Docket swaggerDocket() {
+
 
 		return new Docket(DocumentationType.SWAGGER_2)
 
@@ -36,5 +68,8 @@ public class SandelysApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SandelysApplication.class, args);
 	}
+
+
+
 
 }
